fa1e2064fd035e675b2cc88839826a384ab4192d
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2024-07-01 18:28:09 -0500

    Agregando Alineamiento Global

--- algorithms/alineamiento_global.py
+++ algorithms/alineamiento_global.py
@@ -1,29 +1,47 @@
-def needleman_wunsch_score_matrix(seq1, seq2, match_score, mismatch_score, gap_penalty):
+def needleman_wunsch_score(seq1, seq2, match_score, mismatch_score, gap_penalty):
+    global dp
     len_seq1 = len(seq1)
     len_seq2 = len(seq2)
-    
-    # Inicializar la matriz de puntuaciones y la matriz de rastreo
-    score_matrix = [[0] * (len_seq1 + 1) for _ in range(len_seq2 + 1)]
-    
-    # Inicializar la primera fila y la primera columna con penalizaciones de espacio
+
+    dp = [[0] * (len_seq2 + 1) for _ in range(len_seq1 + 1)]
     for i in range(1, len_seq1 + 1):
-        score_matrix[0][i] = score_matrix[0][i-1] + gap_penalty
+        dp[i][0] = i * gap_penalty
     for j in range(1, len_seq2 + 1):
-        score_matrix[j][0] = score_matrix[j-1][0] + gap_penalty
+        dp[0][j] = j * gap_penalty
+
+    for i in range(1, len_seq1 + 1):
+        for j in range(1, len_seq2 + 1):
+            score = match_score if seq1[i - 1] == seq2[j - 1] else mismatch_score
+            diagonal = dp[i - 1][j - 1] + score
+            left = dp[i][j - 1] + gap_penalty
+            up = dp[i - 1][j] + gap_penalty
+            dp[i][j] = max(diagonal, left, up)
     
-    # Llenar la matriz de puntuaciones
-    for i in range(1, len_seq2 + 1):
-        for j in range(1, len_seq1 + 1):
-            if seq1[j-1] == seq2[i-1]:
-                diagonal_score = score_matrix[i-1][j-1] + match_score
-            else:
-                diagonal_score = score_matrix[i-1][j-1] + mismatch_score
-            
-            left_score = score_matrix[i][j-1] + gap_penalty
-            up_score = score_matrix[i-1][j] + gap_penalty
-            
-            # Determinar el máximo de los puntajes posibles
-            max_score = max(diagonal_score, left_score, up_score)
-            score_matrix[i][j] = max_score
+    final_score = dp[len_seq1][len_seq2]
+    return final_score
+
+
+def backtrack(alignments, seq1, seq2, i, j, alignment_seq1, alignment_seq2, match_score, mismatch_score, gap_penalty):
+    global dp
+    if i == 0 and j == 0:
+        alignments.append((alignment_seq1, alignment_seq2))
+        return
+    if i > 0 and j > 0 and dp[i][j] == dp[i - 1][j - 1] + (match_score if seq1[i - 1] == seq2[j - 1] else mismatch_score):
+        backtrack(alignments, seq1, seq2, i - 1, j - 1, seq1[i - 1] + alignment_seq1, seq2[j - 1] + alignment_seq2, match_score, mismatch_score, gap_penalty)
+    if i > 0 and dp[i][j] == dp[i - 1][j] + gap_penalty:
+        backtrack(alignments, seq1, seq2, i - 1, j, seq1[i - 1] + alignment_seq1, "-" + alignment_seq2, match_score, mismatch_score, gap_penalty)
+    if j > 0 and dp[i][j] == dp[i][j - 1] + gap_penalty:
+        backtrack(alignments, seq1, seq2, i, j - 1, "-" + alignment_seq1, seq2[j - 1] + alignment_seq2, match_score, mismatch_score, gap_penalty)
+
+
+def needleman_wunsch_alignment(seq1, seq2, match_score, mismatch_score, gap_penalty):
+    global dp
+    needleman_wunsch_score(seq1, seq2, match_score, mismatch_score, gap_penalty)
     
-    return score_matrix
+    alignments = []
+    alignment_seq1 = ""
+    alignment_seq2 = ""
+
+    backtrack(alignments, seq1, seq2, len(seq1), len(seq2), alignment_seq1, alignment_seq2, match_score, mismatch_score, gap_penalty)
+    
+    return alignments
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from algorithms.idenntificador_secuencia import identificar_secuencia  # type: i
 from algorithms.transcripcion import transcripcion_adn_arn  # type: ignore
 from algorithms.sub_cadena import buscar_subcadena  # type: ignore
 from algorithms.sub_cadena import calcular_score  # type: ignore
-from algorithms.alineamiento_global import needleman_wunsch_score_matrix  # type: ignore
+from algorithms.alineamiento_global import needleman_wunsch_score,needleman_wunsch_alignment  # type: ignore
 
 
 app = Flask(__name__)
@@ -29,16 +29,7 @@ def analizar():
     operation = request.form.get('operation', '')
 
     result = {}
-    
-    # if operation == 'mayusculas':
-    #     # Llamar a la función de mayúsculas
-    #     result = transform_to_uppercase(sequence1, sequence2)
-    #     result2 = transform_to_lowecase(sequence1, sequence2)
-    #     return render_template('mayusculas.html', result=result, result2=result2, sequence1=sequence1, sequence2=sequence2, operation=operation)
-    # elif operation == 'coincidencias':
-    #     # Llamar a la función de coincidencias
-    #     result = find_coincidences(sequence1, sequence2)
-    #     return render_template('coincidencias.html', result=result, sequence1=sequence1, sequence2=sequence2, operation=operation)
+
     if operation == 'identificar_secuencia':
         # Llamar a la función de identificador de secuencia
         result = identificar_secuencia(sequence)
@@ -52,9 +43,15 @@ def analizar():
         sub_cadena = buscar_subcadena(sequence1, sequence2)
         return render_template('sub_cadena.html', score=score, sub_cadena=sub_cadena, sequence1=sequence1, sequence2=sequence2, operation=operation)
     elif operation == 'needleman_wunsch':
+        # Convertir las entradas a enteros
+        match = int(match)
+        mismatch = int(mismatch)
+        gap = int(gap)
+        
         # Llamar a la función de alineamiento global
-        result = needleman_wunsch_score_matrix(sequence1, sequence2, match, mismatch, gap)
-        return render_template('alineamiento_global.html', result=result, sequence1=sequence1, sequence2=sequence2, operation=operation)
+        score = needleman_wunsch_score(sequence1, sequence2, match, mismatch, gap)
+        alienaciones = needleman_wunsch_alignment(sequence1, sequence2, match, mismatch, gap)
+        return render_template('alineamiento_global.html', score=score, alienaciones=alienaciones, sequence1=sequence1, sequence2=sequence2, match=match, mismatch=mismatch, gap=gap,operation=operation)
     
     return render_template('index.html')
 
